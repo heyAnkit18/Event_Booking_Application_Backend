5d5ec1b8b4fa3b0c21823e864c5b6edc59381eba
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-11-29 20:18:27 +0530

    models,controller and router Added

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express"
 import { dbConnection } from "./database/dbconnection.js";
 import dotenv from "dotenv";
-
+import messageRouter from "./router/messageRouter.js";
 const app = express();
 dotenv.config({ path: "./config/config.env" });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+app.use("/api/v1/message", messageRouter);
 dbConnection();
 
 export default app;
