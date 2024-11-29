44a0e24e976403333caed5a6df7e0d264e68596d
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-11-29 20:46:32 +0530

    cors to connect with frontend

--- app.js
+++ app.js
@@ -2,8 +2,19 @@ import express from "express"
 import { dbConnection } from "./database/dbconnection.js";
 import dotenv from "dotenv";
 import messageRouter from "./router/messageRouter.js";
+
+
 const app = express();
 dotenv.config({ path: "./config/config.env" });
+import cors from "cors";
+
+app.use(
+    cors({
+      origin: [process.env.FRONTEND_URL],
+      methods: ["POST"],
+      credentials: true,
+    })
+  );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
