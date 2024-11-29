4961fd359ccfa1c20370012ddba113c9976d512c
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-11-29 19:53:47 +0530

    Middleware Added

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ import dotenv from "dotenv";
 
 const app = express();
 dotenv.config({ path: "./config/config.env" });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 dbConnection();
 
 export default app;
